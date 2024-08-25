edbf10fc5db595a6c966a29b0fa9ca58976897fc
Kris1027 <obarzanek.work@gmail.com>
2024-08-25 11:38:04 +0200

    create fake data

---
+++ app/fake-data/rooms.ts
@@ -0,0 +1,58 @@
+export const rooms = [
+   {
+      id: 1,
+      created_at: '2024-01-01T00:00:00.000Z',
+      name: 'Room 1',
+      description: 'Room 1 description here',
+      img_urls: [
+         'https://via.placeholder.com/300x200',
+         'https://via.placeholder.com/300x200',
+         'https://via.placeholder.com/300x200',
+      ],
+      regular_price: 100,
+      max_capacity: 2,
+      discount: 0,
+   },
+   {
+      id: 2,
+      created_at: '2024-05-01T00:00:00.000Z',
+      name: 'Room 2',
+      description: 'Room 2 description here',
+      img_urls: [
+         'https://via.placeholder.com/300x200',
+         'https://via.placeholder.com/300x200',
+         'https://via.placeholder.com/300x200',
+      ],
+      regular_price: 400,
+      max_capacity: 6,
+      discount: 50,
+   },
+   {
+      id: 3,
+      created_at: '2024-06-01T00:00:00.000Z',
+      name: 'Room 3',
+      description: 'Room 3 description here',
+      img_urls: [
+         'https://via.placeholder.com/300x200',
+         'https://via.placeholder.com/300x200',
+         'https://via.placeholder.com/300x200',
+      ],
+      regular_price: 200,
+      max_capacity: 4,
+      discount: 100,
+   },
+   {
+      id: 4,
+      created_at: '2024-07-01T00:00:00.000Z',
+      name: 'Room 4',
+      description: 'Room 4 description here',
+      img_urls: [
+         'https://via.placeholder.com/300x200',
+         'https://via.placeholder.com/300x200',
+         'https://via.placeholder.com/300x200',
+      ],
+      regular_price: 500,
+      max_capacity: 7,
+      discount: 100,
+   },
+];
