e69a10db0726fe901f32b537552816a4eaf4e25a
Kris1027 <obarzanek.work@gmail.com>
2024-08-27 13:54:54 +0200

    create types for users and render more user details

--- app/admin-dashboard/users/users-list.tsx
+++ app/admin-dashboard/users/users-list.tsx
@@ -1,13 +1,22 @@
 import { GetUsers } from '@/app/_lib/users-api';
+import { usersProps } from '@/app/types/data-types';
 
 export async function UsersList() {
-   const users = await GetUsers();
+   const users = (await GetUsers()) as usersProps[];
 
    return (
       <div>
-         <ul>
+         <ul className='grid grid-cols-1'>
             {users &&
-               users.map((user) => <li key={user.id}>{user.full_name}</li>)}
+               users.map((user) => (
+                  <li key={user.id}>
+                     <span>{user.id}</span>
+                     <span>{user.created_at}</span>
+                     <span>{user.full_name}</span>
+                     <span>{user.email}</span>
+                     <span>{user.is_admin}</span>
+                  </li>
+               ))}
          </ul>
       </div>
    );
---
+++ app/types/data-types.ts
@@ -0,0 +1,8 @@
+export type usersProps = {
+   id: number;
+   created_at: string;
+   email: string;
+   password: string;
+   is_admin: boolean;
+   full_name: string;
+};
