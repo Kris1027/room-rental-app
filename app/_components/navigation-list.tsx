ad3e78c5fde001d1150b4a15eed88040817a6b4c
Kris1027 <obarzanek.work@gmail.com>
2024-08-29 14:51:08 +0200

    update navigation links with user name and image when is logged in and remove account link when is logged out

--- app/_components/navigation-list.tsx
+++ app/_components/navigation-list.tsx
@@ -1,25 +1,45 @@
 import { NavigationListItem } from '@/app/_components/navigation-list-item';
+import { auth } from '@/auth';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export type NavLinks = {
    name: string;
    path: string;
 };
 
-const navItems: NavLinks[] = [
-   { name: 'Home', path: '/' },
-   { name: 'About', path: '/about' },
-   { name: 'Rooms', path: '/rooms' },
-   { name: 'Account', path: '/account' },
-   { name: 'Admin', path: '/admin-dashboard' },
-];
+export async function NavigationList() {
+   const session = await auth();
+   const userName = session?.user?.name as string;
+   const userImage = session?.user?.image as string;
 
-export function NavigationList() {
+   const navItems: NavLinks[] = [
+      { name: 'Home', path: '/' },
+      { name: 'About', path: '/about' },
+      { name: 'Rooms', path: '/rooms' },
+      { name: 'Admin', path: '/admin-dashboard' },
+   ];
+
    return (
       <nav>
-         <ul className='flex flex-wrap justify-center space-x-4'>
+         <ul className='flex flex-wrap justify-center items-center space-x-4'>
             {navItems.map((item) => (
                <NavigationListItem key={item.name} item={item} />
             ))}
+            {session?.user && (
+               <Link href={'/account'}>
+                  <li className='flex items-center space-x-1'>
+                     <Image
+                        className='rounded-full'
+                        src={userImage}
+                        width={40}
+                        height={40}
+                        alt='profile image'
+                     />
+                     <p>{userName}</p>
+                  </li>
+               </Link>
+            )}
          </ul>
       </nav>
    );
