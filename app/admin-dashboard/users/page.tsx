11f272808205800c859f9742b4494e40c2f26600
Kris1027 <obarzanek.work@gmail.com>
2024-08-29 11:41:40 +0200

    add better styles and responsives for mobile devices in users page at admin dashboard

--- app/admin-dashboard/users/page.tsx
+++ app/admin-dashboard/users/page.tsx
@@ -9,66 +9,93 @@ export default async function AdminUsers() {
    const users = (await GetUsers()) as usersProps[];
 
    return (
-      <div className='overflow-x-auto shadow-md sm:rounded-lg'>
-         <div className='px-6 py-4'>
+      <div className='shadow-md sm:rounded-lg'>
+         <div className='p-4'>
             <Button>Add New User</Button>
          </div>
-         <table className='w-full text-sm text-left text-gray-500'>
-            <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
-               <tr>
-                  <th scope='col' className='px-6 py-3'>
-                     ID
-                  </th>
-                  <th scope='col' className='px-6 py-3'>
-                     Created At
-                  </th>
-                  <th scope='col' className='px-6 py-3'>
-                     Full Name
-                  </th>
-                  <th scope='col' className='px-6 py-3'>
-                     Email
-                  </th>
-                  <th scope='col' className='px-6 py-3'>
-                     Admin
-                  </th>
-               </tr>
-            </thead>
-            <tbody>
-               {users &&
-                  users.map((user) => (
-                     <tr
-                        key={user.id}
-                        className='bg-white border-b hover:bg-gray-50'
-                     >
-                        <td className='px-6 py-4'>{user.id}</td>
-                        <td className='px-6 py-4'>
-                           {formatDateTime(user.created_at)}
-                        </td>
-                        <td className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
-                           {user.full_name}
-                        </td>
-                        <td className='px-6 py-4'>{user.email}</td>
-                        <td className='px-6 py-4'>
-                           <span
-                              className={`px-2 py-1 rounded text-xs font-medium ${
-                                 user.is_admin
-                                    ? 'bg-green-100 text-green-800'
-                                    : 'bg-red-100 text-red-800'
-                              }`}
-                           >
-                              {user.is_admin ? 'Yes' : 'No'}
-                           </span>
-                        </td>
-                        <td className='space-x-1'>
-                           <Button size='small'>Edit</Button>
-                           <Button size='small' type='submit'>
-                              Delete
-                           </Button>
-                        </td>
-                     </tr>
-                  ))}
-            </tbody>
-         </table>
+         <div className='overflow-x-auto'>
+            <table className='w-full text-sm text-left text-gray-500'>
+               <thead className='text-xs text-gray-700 uppercase bg-gray-50 hidden md:table-header-group'>
+                  <tr>
+                     <th scope='col' className='px-4 py-2'>
+                        ID
+                     </th>
+                     <th scope='col' className='px-4 py-2'>
+                        Created At
+                     </th>
+                     <th scope='col' className='px-4 py-2'>
+                        Full Name
+                     </th>
+                     <th scope='col' className='px-4 py-2'>
+                        Email
+                     </th>
+                     <th scope='col' className='px-4 py-2'>
+                        Admin
+                     </th>
+                     <th scope='col' className='px-4 py-2'>
+                        Actions
+                     </th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {users &&
+                     users.map((user) => (
+                        <tr
+                           key={user.id}
+                           className='bg-white border-b hover:bg-gray-50 flex flex-col md:table-row'
+                        >
+                           <td className='flex justify-between md:table-cell px-4 py-2'>
+                              <span className='font-bold md:hidden'>ID:</span>{' '}
+                              {user.id}
+                           </td>
+                           <td className='flex justify-between md:table-cell px-4 py-2'>
+                              <span className='font-bold md:hidden'>
+                                 Created At:
+                              </span>{' '}
+                              {formatDateTime(user.created_at)}
+                           </td>
+                           <td className='flex justify-between md:table-cell px-4 py-2 font-medium text-gray-900'>
+                              <span className='font-bold md:hidden'>
+                                 Full Name:
+                              </span>{' '}
+                              {user.full_name}
+                           </td>
+                           <td className='flex justify-between md:table-cell px-4 py-2'>
+                              <span className='font-bold md:hidden'>
+                                 Email:
+                              </span>{' '}
+                              {user.email}
+                           </td>
+                           <td className='flex justify-between md:table-cell px-4 py-2'>
+                              <span className='font-bold md:hidden'>
+                                 Admin:
+                              </span>
+                              <span
+                                 className={`px-2 py-1 rounded text-xs font-medium ${
+                                    user.is_admin
+                                       ? 'bg-green-100 text-green-800'
+                                       : 'bg-red-100 text-red-800'
+                                 }`}
+                              >
+                                 {user.is_admin ? 'Yes' : 'No'}
+                              </span>
+                           </td>
+                           <td className='flex justify-between md:table-cell px-4 py-2'>
+                              <span className='font-bold md:hidden'>
+                                 Actions:
+                              </span>
+                              <div className='space-y-1 space-x-1 text-center'>
+                                 <Button size='small'>Edit</Button>
+                                 <Button size='small' type='submit'>
+                                    Delete
+                                 </Button>
+                              </div>
+                           </td>
+                        </tr>
+                     ))}
+               </tbody>
+            </table>
+         </div>
       </div>
    );
 }
