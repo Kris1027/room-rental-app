a3da04e1ce4d42dd611bf69776c23bca9003c920
Kris1027 <obarzanek.work@gmail.com>
2024-09-03 11:24:08 +0200

    add style for user form

--- app/admin-dashboard/users/user-form.tsx
+++ app/admin-dashboard/users/user-form.tsx
@@ -1,11 +1,47 @@
+import { Button } from '@/app/_components/button';
 import { adminCreateUserAction } from '@/app/_lib/actions';
 
 export function UserForm() {
    return (
-      <form action={adminCreateUserAction}>
-         <input type='text' name='full_name' placeholder='Full Name' required />
-         <input type='email' name='email' placeholder='Email' required />
-         <button type='submit'>Add New User</button>
+      <form
+         action={adminCreateUserAction}
+         className='max-w-lg mx-auto p-6 bg-white shadow-md rounded-lg space-y-4'
+      >
+         <div className='flex flex-col'>
+            <label
+               htmlFor='full_name'
+               className='text-sm font-medium text-gray-700'
+            >
+               Full Name
+            </label>
+            <input
+               type='text'
+               name='full_name'
+               id='full_name'
+               placeholder='Full Name'
+               required
+               className='mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
+            />
+         </div>
+         <div className='flex flex-col'>
+            <label
+               htmlFor='email'
+               className='text-sm font-medium text-gray-700'
+            >
+               Email
+            </label>
+            <input
+               type='email'
+               name='email'
+               id='email'
+               placeholder='Email'
+               required
+               className='mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
+            />
+         </div>
+         <Button type='submit' fullWidth>
+            Add New User
+         </Button>
       </form>
    );
 }
