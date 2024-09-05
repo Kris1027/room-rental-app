d11f169ee5d53bb234dd9872c4ceb92285344400
Kris1027 <obarzanek.work@gmail.com>
2024-09-05 11:34:38 +0200

    build user update api and start to build user update form

--- app/_lib/users-api.ts
+++ app/_lib/users-api.ts
@@ -1,4 +1,5 @@
 import { supabase } from '@/app/_lib/supabase';
+import { usersProps } from '@/app/types/data-types';
 
 export async function getUsers() {
    const { data: users, error } = await supabase.from('users').select('*');
@@ -34,4 +35,20 @@ export async function createUser(newUser: {
 
       return data;
    }
+}
+
+export async function updateUser(id: number, updatedFields: usersProps[]) {
+   const { data, error } = await supabase
+      .from('users')
+      .update(updatedFields)
+      .eq('id', id)
+      .select()
+      .single();
+
+   if (error) {
+      console.error(error);
+      throw new Error('User could not be updated');
+   }
+
+   return data;
 }
