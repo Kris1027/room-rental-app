431209ff5897810a5e7c4efcd16660040a9d8b18
Kris1027 <obarzanek.work@gmail.com>
2024-09-09 15:22:17 +0200

    build rooms-columns component

--- app/admin-dashboard/rooms/page.tsx
+++ app/admin-dashboard/rooms/page.tsx
@@ -6,6 +6,7 @@ import { roomsProps } from '@/app/types/data-types';
 import { auth } from '@/auth';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
+import { RoomsColumns } from '@/app/admin-dashboard/rooms/rooms-columns';
 
 export const revalidate = 0;
 
@@ -25,37 +26,7 @@ export default async function AdminRooms() {
          </div>
          <div className='hidden sm:block'>
             <table className='w-full text-sm text-left text-gray-500'>
-               <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
-                  <tr>
-                     <th scope='col' className='px-3 py-2'>
-                        ID
-                     </th>
-                     <th scope='col' className='px-3 py-2'>
-                        Created At
-                     </th>
-                     <th scope='col' className='px-3 py-2'>
-                        Image
-                     </th>
-                     <th scope='col' className='px-3 py-2'>
-                        Name
-                     </th>
-                     <th scope='col' className='px-3 py-2'>
-                        Description
-                     </th>
-                     <th scope='col' className='px-3 py-2'>
-                        Regular price
-                     </th>
-                     <th scope='col' className='px-3 py-2'>
-                        Max capacity
-                     </th>
-                     <th scope='col' className='px-3 py-2'>
-                        Discount
-                     </th>
-                     <th scope='col' className='px-3 py-2'>
-                        Actions
-                     </th>
-                  </tr>
-               </thead>
+               <RoomsColumns />
                <tbody>
                   {rooms &&
                      rooms.map((room) => (
@@ -132,7 +103,7 @@ export default async function AdminRooms() {
                         <strong>Discount:</strong>{' '}
                         {room.discount === null ? 0 : room.discount}
                      </div>
-                     <div className='flex justify-end space-x-2'>
+                     <div className='flex gap-1 w-full'>
                         <Button size='small'>Edit</Button>
                         <Button size='small' type='submit'>
                            Delete
