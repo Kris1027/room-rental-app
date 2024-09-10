ae44570a3e250b29703d8ff74acf19422e914a3f
Kris1027 <obarzanek.work@gmail.com>
2024-09-10 10:54:23 +0200

    build reservations list component

---
+++ app/admin-dashboard/reservations/reservations-list.tsx
@@ -0,0 +1,20 @@
+import { ReservationDetails } from '@/app/admin-dashboard/reservations/reservation-details';
+import { type reservationsProps } from '@/app/types/data-types';
+
+export function ReservationsList({
+   reservations,
+}: {
+   reservations: reservationsProps[];
+}) {
+   return (
+      <tbody>
+         {reservations &&
+            reservations.map((reservation) => (
+               <ReservationDetails
+                  key={reservation.id}
+                  reservation={reservation}
+               />
+            ))}
+      </tbody>
+   );
+}
