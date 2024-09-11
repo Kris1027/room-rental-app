acd5f5210f4448b4059b3ddc18d195a09f38fdc7
Kris1027 <obarzanek.work@gmail.com>
2024-09-11 13:16:36 +0200

    add styles and build minEndDate function

--- app/admin-dashboard/reservations/reservation-datepicker.tsx
+++ app/admin-dashboard/reservations/reservation-datepicker.tsx
@@ -1,12 +1,30 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export function ReservationDatePicker() {
    const [startDate, setStartDate] = useState(new Date());
-   const [endDate, setEndDate] = useState(new Date());
+   const [endDate, setEndDate] = useState<Date | null>(null);
+
+   useEffect(() => {
+      setEndDate(new Date(startDate.getTime() + 2 * 24 * 60 * 60 * 1000));
+   }, [startDate]);
+
+   const handleStartDateChange = (date: Date | null) => {
+      if (date) {
+         setStartDate(date);
+         const minEndDate = new Date(date.getTime() + 2 * 24 * 60 * 60 * 1000);
+         if (!endDate || endDate < minEndDate) {
+            setEndDate(minEndDate);
+         }
+      }
+   };
+
+   const getMinEndDate = () => {
+      return new Date(startDate.getTime() + 2 * 24 * 60 * 60 * 1000);
+   };
 
    return (
       <>
@@ -18,10 +36,11 @@ export function ReservationDatePicker() {
                Start date
             </label>
             <DatePicker
+               className='w-full px-3 my-1 py-2 border border-neutral-300 rounded-md shadow-sm cursor-pointer'
                selected={startDate}
-               onChange={(date) => date && setStartDate(date)}
+               onChange={handleStartDateChange}
                dateFormat='dd/MM/yyyy'
-               minDate={startDate}
+               minDate={new Date()}
             />
          </div>
          <div className='flex flex-col'>
@@ -32,10 +51,11 @@ export function ReservationDatePicker() {
                End date
             </label>
             <DatePicker
+               className='w-full px-3 my-1 py-2 border border-neutral-300 rounded-md shadow-sm cursor-pointer'
                selected={endDate}
                onChange={(date) => date && setEndDate(date)}
                dateFormat='dd/MM/yyyy'
-               minDate={startDate}
+               minDate={getMinEndDate()}
             />
          </div>
       </>
