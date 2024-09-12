dc8e8daa7b4ec0050a1e64cfd9a6be6a91ec5961
Kris1027 <obarzanek.work@gmail.com>
2024-09-12 13:22:11 +0200

    change logo colors

--- app/_components/logo.tsx
+++ app/_components/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Logo() {
    return (
       <Link href='/' className='text-3xl font-extrabold mb-4 md:mb-0'>
-         <h1 className='bg-gradient-to-r from-blue-500 to-teal-400 bg-clip-text text-transparent'>
+         <h1 className='bg-gradient-to-r from-primary to-dangerHover bg-clip-text text-transparent'>
             RoomRental
             <span className='text-sm font-semibold ml-1'>App</span>
          </h1>
