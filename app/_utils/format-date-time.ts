0828303c2968cebf130085167f7131f69de427b6
Kris1027 <obarzanek.work@gmail.com>
2024-09-15 23:16:06 +0200

    update colors and using formatDateTime from utils i create before, also update formatDateTime function

--- app/_utils/format-date-time.ts
+++ app/_utils/format-date-time.ts
@@ -2,10 +2,7 @@ export function formatDateTime(dateString: string) {
    const date = new Date(dateString);
    return date.toLocaleString('en-GB', {
       year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      hour12: true,
+      month: 'short',
+      day: 'numeric',
    });
 }
--- app/account/reservations/page.tsx
+++ app/account/reservations/page.tsx
@@ -1,12 +1,13 @@
 import { getReservationsByUserId } from '@/app/_lib/reservations-api';
+import { formatDateTime } from '@/app/_utils/format-date-time';
 import { reservationsProps } from '@/app/types/data-types';
 import { auth } from '@/auth';
 import {
    FaCalendarAlt,
-   FaUsers,
-   FaMoon,
    FaDollarSign,
    FaExclamationCircle,
+   FaMoon,
+   FaUsers,
 } from 'react-icons/fa';
 
 export default async function UserReservations() {
@@ -16,15 +17,6 @@ export default async function UserReservations() {
    const reservations = (await getReservationsByUserId(
       Number(userId)
    )) as reservationsProps[];
-
-   function formatDate(dateString: string) {
-      const date = new Date(dateString);
-      return date.toLocaleDateString('en-US', {
-         year: 'numeric',
-         month: 'short',
-         day: 'numeric',
-      });
-   }
 
    return (
       <div className='container mx-auto px-4 py-8'>
@@ -37,46 +29,46 @@ export default async function UserReservations() {
                   key={reservation.id}
                   className='bg-white rounded-lg shadow-md overflow-hidden'
                >
-                  <div className='bg-blue-600 text-white px-4 py-2'>
+                  <div className='bg-gradient-to-r from-primaryHover to-dangerHover text-black px-4 py-2'>
                      <h2 className='text-xl font-semibold'>
                         Reservation #{reservation.id}
                      </h2>
                   </div>
                   <div className='p-4'>
                      <p className='text-sm text-gray-600 mb-2'>
-                        Created on {formatDate(reservation.created_at)}
+                        Created on {formatDateTime(reservation.created_at)}
                      </p>
                      <div className='flex items-center mb-2'>
-                        <FaCalendarAlt className='w-5 h-5 mr-2 text-blue-600' />
+                        <FaCalendarAlt className='w-5 h-5 mr-2 text-primaryHover' />
                         <p>
-                           {formatDate(reservation.start_date)} -{' '}
-                           {formatDate(reservation.end_date)}
+                           {formatDateTime(reservation.start_date)} -{' '}
+                           {formatDateTime(reservation.end_date)}
                         </p>
                      </div>
                      <div className='flex items-center mb-2'>
-                        <FaMoon className='w-5 h-5 mr-2 text-blue-600' />
+                        <FaMoon className='w-5 h-5 mr-2 text-primaryHover' />
                         <p>
                            {reservation.num_nights} night
                            {reservation.num_nights > 1 ? 's' : ''}
                         </p>
                      </div>
                      <div className='flex items-center mb-2'>
-                        <FaUsers className='w-5 h-5 mr-2 text-blue-600' />
+                        <FaUsers className='w-5 h-5 mr-2 text-primaryHover' />
                         <p>
                            {reservation.num_guests} guest
                            {reservation.num_guests > 1 ? 's' : ''}
                         </p>
                      </div>
                      <div className='flex items-center mb-2'>
-                        <FaDollarSign className='w-5 h-5 mr-2 text-blue-600' />
+                        <FaDollarSign className='w-5 h-5 mr-2 text-primaryHover' />
                         <p>Total: ${reservation.total_price.toFixed(2)}</p>
                      </div>
                      <div className='flex items-center'>
-                        <FaExclamationCircle className='w-5 h-5 mr-2 text-blue-600' />
+                        <FaExclamationCircle className='w-5 h-5 mr-2 text-primaryHover' />
                         <p>Status: {reservation.status}</p>
                      </div>
                   </div>
-                  <div className='bg-gray-100 px-4 py-3 mt-4'>
+                  <div className='bg-gradient-to-r from-primaryHover to-dangerHover px-4 py-3 mt-4'>
                      <p
                         className={`text-sm font-semibold ${
                            reservation.is_paid
