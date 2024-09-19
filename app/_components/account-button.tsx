68547725231d7fe3b52f46fab7d91375958c37fb
Kris1027 <obarzanek.work@gmail.com>
2024-09-19 14:11:00 +0200

    change to use full name from database, not from session

--- app/_components/account-button.tsx
+++ app/_components/account-button.tsx
@@ -1,10 +1,15 @@
+import { getUserById } from '@/app/_lib/users-api';
+import type { usersProps } from '@/app/types/data-types';
 import { auth } from '@/auth';
-import Image from 'next/image';
 import { Link } from 'next-view-transitions';
+import Image from 'next/image';
 
 export async function AccountButton() {
    const session = await auth();
-   const userName = session?.user?.name as string;
+   const userId = session?.user.userId;
+   const user = (await getUserById(Number(userId))) as usersProps;
+
+   const userName = user.full_name;
    const userImage = session?.user?.image as string;
 
    return (
