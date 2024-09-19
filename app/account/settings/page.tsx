58c8d919e91d8e2693b678db0bbf332a6eb872a3
Kris1027 <obarzanek.work@gmail.com>
2024-09-19 13:54:54 +0200

    add text center to heading

--- app/account/settings/page.tsx
+++ app/account/settings/page.tsx
@@ -11,7 +11,7 @@ export default async function Settings() {
 
    return (
       <div className='container mx-auto px-4 py-8'>
-         <h2 className='text-3xl font-bold mb-6 text-gray-800'>
+         <h2 className='text-3xl font-bold mb-6 text-gray-800 text-center'>
             Account Settings
          </h2>
          <form className='space-y-6'>
