cf9d001fee7186ae1dfd7ef59daba801e09254c4
Kris1027 <obarzanek.work@gmail.com>
2024-09-22 18:25:22 +0200

    add metadata and fix style

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,18 +1,23 @@
 import { BackButton } from '@/app/_components/back-button';
 import { SignOutButton } from '@/app/_components/sign-out-button';
+import type { Metadata } from 'next';
 import { FiAlertCircle } from 'react-icons/fi';
+
+export const metadata: Metadata = {
+   title: 'Logout',
+};
 
 export default function Logout() {
    return (
       <main className='flex-1 w-full max-w-7xl mx-auto p-1 md:p-4 flex justify-center items-center'>
-         <div className='bg-white rounded-lg shadow-lg p-8 max-w-md w-full mx-4'>
-            <div className='flex items-center mb-6'>
-               <FiAlertCircle className='text-yellow-500 text-3xl mr-3' />
-               <h2 className='text-2xl font-bold text-gray-800'>
+         <div className='bg-white rounded-lg shadow-lg p-8 max-w-xl w-full mx-4'>
+            <div className='flex items-center justify-center mb-6'>
+               <FiAlertCircle size={64} className='text-yellow-500 mr-3' />
+               <h2 className='text-3xl font-bold text-gray-800'>
                   Confirm Logout
                </h2>
             </div>
-            <p className='text-gray-600 mb-8'>
+            <p className='text-gray-600 mb-8 text-center'>
                Are you sure you want to log out? You will need to sign in again
                to access your account.
             </p>
