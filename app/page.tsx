d2576eea43742a4f3c668c08287917c7f55688e0
Kris1027 <obarzanek.work@gmail.com>
2024-09-23 14:49:17 +0200

    working with padding in mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import { Amenities } from "@/app/_components/amenities";
-import { BookLink } from "@/app/_components/book-link";
-import { GuestTestimonials } from "@/app/_components/guest-testimonials";
-import { Hero } from "@/app/_components/hero";
-import { HomeAbout } from "@/app/_components/home-about";
+import { Amenities } from '@/app/_components/amenities';
+import { BookLink } from '@/app/_components/book-link';
+import { GuestTestimonials } from '@/app/_components/guest-testimonials';
+import { Hero } from '@/app/_components/hero';
+import { HomeAbout } from '@/app/_components/home-about';
 
 export default function Home() {
-	return (
-		<main className="flex-1 w-full max-w-7xl mx-auto p-1 md:p-4">
-			<Hero />
-			<HomeAbout />
-			<Amenities />
-			<GuestTestimonials />
-			<BookLink />
-		</main>
-	);
+   return (
+      <main className='flex-1 w-full max-w-7xl mx-auto p-0 lg:p-4 md:p-4'>
+         <Hero />
+         <HomeAbout />
+         <Amenities />
+         <GuestTestimonials />
+         <BookLink />
+      </main>
+   );
 }
