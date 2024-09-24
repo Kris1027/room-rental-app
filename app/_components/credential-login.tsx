01d53a90e1dfab69616b5c22780c475b8d491e21
Kris1027 <obarzanek.work@gmail.com>
2024-09-24 17:49:22 +0200

    remove default values

--- app/_components/credential-login.tsx
+++ app/_components/credential-login.tsx
@@ -19,7 +19,6 @@ export function CredentialLogin() {
                name='email'
                id='email'
                placeholder='Email'
-               defaultValue='kris@kris.pl'
                required
                className='mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
             />
@@ -36,7 +35,6 @@ export function CredentialLogin() {
                name='password'
                id='password'
                placeholder='Password'
-               defaultValue='kris123'
                required
                className='mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500'
             />
