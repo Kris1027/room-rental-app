7addf36d318235785b44b53b480785afb39b2a25
Kris1027 <obarzanek.work@gmail.com>
2024-09-25 20:20:09 +0200

    responsive loadings

--- app/admin-dashboard/loading.tsx
+++ app/admin-dashboard/loading.tsx
@@ -1,21 +1,21 @@
-import { FaSpinner } from "react-icons/fa";
+import { FaSpinner } from 'react-icons/fa';
 
 export default function Loading() {
-	return (
-		<main className="flex flex-col items-center justify-center flex-1 w-full max-w-7xl mx-auto p-1 md:p-4">
-			<h2 className="text-4xl font-serif text-amber-800 mb-8 text-center">
-				Admin Dashboard
-			</h2>
-			<div className="relative w-24 h-24 mb-8">
-				<div className="absolute inset-0 border-4 border-amber-300 rounded-full animate-ping"></div>
-				<div
-					className="absolute inset-2 border-4 border-amber-500 rounded-full animate-ping"
-					style={{ animationDelay: "-0.5s" }}
-				></div>
-				<div className="absolute inset-0 flex items-center justify-center">
-					<FaSpinner className="w-12 h-12 text-amber-700 animate-spin" />
-				</div>
-			</div>
-		</main>
-	);
+   return (
+      <main className='flex flex-col items-center justify-center flex-1 w-full max-w-7xl mx-auto p-4 min-h-screen'>
+         <h2 className='text-2xl md:text-4xl font-serif text-amber-800 mb-4 md:mb-8 text-center'>
+            Admin Dashboard
+         </h2>
+         <div className='relative w-16 h-16 md:w-24 md:h-24 mb-4 md:mb-8'>
+            <div className='absolute inset-0 border-4 border-amber-300 rounded-full animate-ping'></div>
+            <div
+               className='absolute inset-1 md:inset-2 border-4 border-amber-500 rounded-full animate-ping'
+               style={{ animationDelay: '-0.5s' }}
+            ></div>
+            <div className='absolute inset-0 flex items-center justify-center'>
+               <FaSpinner className='w-8 h-8 md:w-12 md:h-12 text-amber-700 animate-spin' />
+            </div>
+         </div>
+      </main>
+   );
 }
