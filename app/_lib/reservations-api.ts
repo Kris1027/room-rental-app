f4fe4066f64c9d02d352282d88c0ecc1216dedce
Kris1027 <obarzanek.work@gmail.com>
2024-09-28 18:10:17 +0200

    change from double quotes to single

--- app/_lib/reservations-api.ts
+++ app/_lib/reservations-api.ts
@@ -1,57 +1,57 @@
-import { supabase } from "@/app/_lib/supabase";
+import { supabase } from '@/app/_lib/supabase';
 
 export async function getReservations() {
-	const { data: reservations, error } = await supabase
-		.from("reservations")
-		.select("*");
+   const { data: reservations, error } = await supabase
+      .from('reservations')
+      .select('*');
 
-	if (error) {
-		console.error("Error fetching users:", error);
-		return null;
-	}
+   if (error) {
+      console.error('Error fetching users:', error);
+      return null;
+   }
 
-	return reservations;
+   return reservations;
 }
 
 export async function createReservation(newReservation: {
-	user_id: number;
-	room_id: number;
-	start_date: string;
-	end_date: string;
+   user_id: number;
+   room_id: number;
+   start_date: string;
+   end_date: string;
 }) {
-	{
-		const { data, error } = await supabase
-			.from("reservations")
-			.insert([newReservation]);
+   {
+      const { data, error } = await supabase
+         .from('reservations')
+         .insert([newReservation]);
 
-		if (error) {
-			console.error(error);
-			throw new Error("Reservation could not be created");
-		}
+      if (error) {
+         console.error(error);
+         throw new Error('Reservation could not be created');
+      }
 
-		return data;
-	}
+      return data;
+   }
 }
 
 export async function getReservation(id: number) {
-	const { data } = await supabase
-		.from("reservations")
-		.select("*")
-		.eq("id", id)
-		.single();
-	return data;
+   const { data } = await supabase
+      .from('reservations')
+      .select('*')
+      .eq('id', id)
+      .single();
+   return data;
 }
 
 export async function getReservationsByUserId(userId: number) {
-	const { data: reservations, error } = await supabase
-		.from("reservations")
-		.select("*")
-		.eq("user_id", userId);
+   const { data: reservations, error } = await supabase
+      .from('reservations')
+      .select('*')
+      .eq('user_id', userId);
 
-	if (error) {
-		console.error("Error fetching users:", error);
-		return null;
-	}
+   if (error) {
+      console.error('Error fetching users:', error);
+      return null;
+   }
 
-	return reservations;
+   return reservations;
 }
