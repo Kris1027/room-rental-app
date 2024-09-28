80ef75d5757a8153d00826d1e9202c9f7f577780
Kris1027 <obarzanek.work@gmail.com>
2024-09-28 09:29:53 +0200

    update users columns

--- app/admin-dashboard/users/users-columns.tsx
+++ app/admin-dashboard/users/users-columns.tsx
@@ -1,15 +1,15 @@
 export function UsersColumns() {
-	return (
-		<thead className="text-xs text-gray-700 uppercase bg-gray-50 hidden md:table-header-group">
-			<tr>
-				{["ID", "Created At", "Full Name", "Email", "Is Admin", "Actions"].map(
-					(header) => (
-						<th key={header} scope="col" className="px-4 py-2">
-							{header}
-						</th>
-					),
-				)}
-			</tr>
-		</thead>
-	);
+   return (
+      <thead className='text-xs text-gray-700 uppercase bg-gray-50 hidden md:table-header-group text-center'>
+         <tr>
+            {['ID', 'Created At', 'Full Name', 'Email', 'Is Admin', ''].map(
+               (header) => (
+                  <th key={header} scope='col' className='px-4 py-2'>
+                     {header}
+                  </th>
+               )
+            )}
+         </tr>
+      </thead>
+   );
 }
