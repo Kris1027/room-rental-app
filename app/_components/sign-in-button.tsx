2a645e82f8f905c2fd16baba9f3c9d83b38409bf
Kris1027 <obarzanek.work@gmail.com>
2024-10-01 10:23:16 +0200

    fix responsive for google image

--- app/_components/sign-in-button.tsx
+++ app/_components/sign-in-button.tsx
@@ -6,7 +6,7 @@ export function SignInButton() {
       <form action={signInAction} className='mx-auto'>
          <button
             type='submit'
-            className='flex items-center gap-2 lg:gap-6 text-sm lg:text-lg border border-primary-300 px-5 lg:px-10 py-2 lg:py-4 font-medium'
+            className='flex items-center gap-2 lg:gap-6 text-sm lg:text-lg border border-primary-300 px-4 lg:px-10 py-2 lg:py-4 font-medium'
          >
             <div className='relative'>
                <Image
@@ -15,7 +15,7 @@ export function SignInButton() {
                   width={0}
                   height={0}
                   sizes='100vw'
-                  className='w-full h-10 object-cover'
+                  className='w-full h-6 md:h-10 object-cover'
                />
             </div>
             <span>Continue with Google</span>
