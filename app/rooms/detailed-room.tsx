889aada52b462e5d8dae8eae3152ed5e442d0df6
Kris1027 <obarzanek.work@gmail.com>
2024-10-18 00:49:37 +0200

    change details link style

--- app/rooms/detailed-room.tsx
+++ app/rooms/detailed-room.tsx
@@ -1,81 +1,81 @@
-import { BackButton } from '@/app/_components/back-button';
-import { UserReservationForm } from '@/app/rooms/user-reservation-form';
-import type { roomsProps } from '@/app/types/data-types';
-import { auth } from '@/auth';
+import {BackButton} from '@/app/_components/back-button';
+import {UserReservationForm} from '@/app/rooms/user-reservation-form';
+import type {roomsProps} from '@/app/types/data-types';
+import {auth} from '@/auth';
 import Image from 'next/image';
-import { FaDollarSign, FaTag, FaUserFriends } from 'react-icons/fa';
+import {FaDollarSign, FaTag, FaUserFriends} from 'react-icons/fa';
 import {RoomLoginButton} from "@/app/rooms/room-login-button";
 
-export async function DetailedRoom({ room }: { room: roomsProps }) {
-   const session = await auth();
-   const userId = session?.user?.userId;
+export async function DetailedRoom({room}: { room: roomsProps }) {
+    const session = await auth();
+    const userId = session?.user?.userId;
 
-   return (
-      <div className='flex flex-col py-2 gap-4 items-start justify-center'>
-         <BackButton />
-         <div className='bg-white p-2 lg:p-4 rounded-xl shadow-lg max-w-3xl mx-auto'>
-            <div className='flex flex-col md:flex-row gap-6'>
-               <div className='relative'>
-                  <Image
-                     src={room.image_url}
-                     alt={room.name}
-                     width={0}
-                     height={0}
-                     sizes='100vw'
-                     className='w-full h-96 object-cover rounded-t-lg lg:rounded-lg'
-                  />
-               </div>
-
-               <div className='flex flex-col justify-between w-full md:w-1/2'>
-                  <div>
-                     <h2 className='text-3xl font-bold text-gray-800 mb-3'>
-                        {room.name}
-                     </h2>
-                     <p className='text-gray-600 mb-4 text-sm md:text-base leading-relaxed'>
-                        {room.description}
-                     </p>
-                  </div>
-
-                  <div className='grid grid-cols-1 gap-2 md:gap-4'>
-                     <div className='flex items-center text-gray-800 font-bold text-lg'>
-                        <FaDollarSign
-                           size={24}
-                           className='mr-2 text-green-600'
+    return (
+        <div className='flex flex-col py-2 gap-4 items-start justify-center'>
+            <BackButton/>
+            <div className='bg-white p-2 lg:p-4 rounded-xl shadow-lg max-w-3xl mx-auto'>
+                <div className='flex flex-col md:flex-row gap-6'>
+                    <div className='relative'>
+                        <Image
+                            src={room.image_url}
+                            alt={room.name}
+                            width={0}
+                            height={0}
+                            sizes='100vw'
+                            className='w-full h-96 object-cover rounded-t-lg lg:rounded-lg'
                         />
-                        <span>Price: ${room.regular_price}</span>
-                     </div>
+                    </div>
 
-                     <div className='flex items-center text-gray-800'>
-                        <FaUserFriends
-                           size={24}
-                           className='mr-2 text-blue-500'
-                        />
-                        <span>
-                           Max Capacity: <strong>{room.max_capacity}</strong>{' '}
-                           people
-                        </span>
-                     </div>
-
-                     {room.discount > 0 && (
-                        <div className='flex items-center text-green-600 font-semibold'>
-                           <FaTag size={24} className='mr-2' />
-                           <span>Discount: ${room.discount}</span>
+                    <div className='flex flex-col justify-between w-full md:w-1/2'>
+                        <div>
+                            <h2 className='text-3xl font-bold text-gray-800 mb-3'>
+                                {room.name}
+                            </h2>
+                            <p className='text-gray-600 mb-4 text-sm md:text-base leading-relaxed'>
+                                {room.description}
+                            </p>
                         </div>
-                     )}
-                  </div>
-               </div>
-            </div>
 
-            {session?.user && userId ? (
-               <div className='mt-6'>
-                  <UserReservationForm room={room} userId={Number(userId)} />
-               </div>
-            ):
-                <div className='mt-6'>
-                   <RoomLoginButton />
+                        <div className='grid grid-cols-1 gap-2 md:gap-4'>
+                            <div className='flex items-center text-gray-800 font-bold text-lg'>
+                                <FaDollarSign
+                                    size={24}
+                                    className='mr-2 text-green-600'
+                                />
+                                <span>Price: ${room.regular_price}</span>
+                            </div>
+
+                            <div className='flex items-center text-gray-800'>
+                                <FaUserFriends
+                                    size={24}
+                                    className='mr-2 text-blue-500'
+                                />
+                                <span>
+                           Max Capacity: <strong>{room.max_capacity}</strong>{' '}
+                                    people
+                        </span>
+                            </div>
+
+                            {room.discount > 0 && (
+                                <div className='flex items-center text-green-600 font-semibold'>
+                                    <FaTag size={24} className='mr-2'/>
+                                    <span>Discount: ${room.discount}</span>
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
+
+                {session?.user && userId ? (
+                        <div className='mt-6'>
+                            <UserReservationForm room={room} userId={Number(userId)}/>
+                        </div>
+                    ) :
+                    <div className='mt-6'>
+                        <RoomLoginButton/>
+                    </div>
                 }
-         </div>
-      </div>
-   );
+            </div>
+        </div>
+    );
 }
--- app/rooms/rooms-list-item.tsx
+++ app/rooms/rooms-list-item.tsx
@@ -1,65 +1,66 @@
-import { Button } from '@/app/_components/button';
-import type { roomsProps } from '@/app/types/data-types';
-import { Link } from 'next-view-transitions';
+import type {roomsProps} from '@/app/types/data-types';
+import {Link} from 'next-view-transitions';
 import Image from 'next/image';
-import { FaDollarSign, FaEye, FaTag, FaUserFriends } from 'react-icons/fa';
+import {FaDollarSign, FaEye, FaTag, FaUserFriends} from 'react-icons/fa';
 
-export function RoomsListItem({ room }: { room: roomsProps }) {
-   return (
-      <div className='bg-white border rounded-lg overflow-hidden shadow-lg transition-transform transform lg:hover:scale-105 duration-300 flex flex-col w-full'>
-         <div className='relative'>
-            <Image
-               src={room.image_url}
-               alt={room.name}
-               width={0}
-               height={0}
-               sizes='100vw'
-               className='w-full h-48 object-cover'
-            />
-         </div>
-         <div className='p-5 flex-grow flex flex-col'>
-            <h2 className='text-xl font-bold text-gray-800 mb-3'>
-               {room.name}
-            </h2>
-            <div className='space-y-3'>
-               <div className='flex justify-between items-center text-gray-800 font-medium'>
-                  <div className='flex items-center'>
-                     <FaDollarSign size={24} className='mr-2 text-green-600' />
-                     <span>Price:</span>
-                  </div>
-                  <span className='font-bold text-gray-900'>
+export function RoomsListItem({room}: { room: roomsProps }) {
+    return (
+        <div
+            className='bg-white border rounded-lg overflow-hidden shadow-lg transition-transform transform lg:hover:scale-105 duration-300 flex flex-col w-full'>
+            <div className='relative'>
+                <Image
+                    src={room.image_url}
+                    alt={room.name}
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    className='w-full h-48 object-cover'
+                />
+            </div>
+            <div className='p-5 flex-grow flex flex-col'>
+                <h2 className='text-xl font-bold text-gray-800 mb-3'>
+                    {room.name}
+                </h2>
+                <div className='space-y-3'>
+                    <div className='flex justify-between items-center text-gray-800 font-medium'>
+                        <div className='flex items-center'>
+                            <FaDollarSign size={24} className='mr-2 text-green-600'/>
+                            <span>Price:</span>
+                        </div>
+                        <span className='font-bold text-gray-900'>
                      {room.regular_price} $
                   </span>
-               </div>
-               <div className='flex justify-between items-center text-gray-800 font-medium'>
-                  <div className='flex items-center'>
-                     <FaUserFriends size={24} className='mr-2 text-blue-500' />
-                     <span>Max Capacity:</span>
-                  </div>
-                  <span>{room.max_capacity}</span>
-               </div>
-               {room.discount > 0 && (
-                  <div className='flex justify-between items-center text-green-600 font-semibold'>
-                     <div className='flex items-center'>
-                        <FaTag size={24} className='mr-2' />
-                        <span>Discount:</span>
-                     </div>
-                     <span>{room.discount}$</span>
-                  </div>
-               )}
+                    </div>
+                    <div className='flex justify-between items-center text-gray-800 font-medium'>
+                        <div className='flex items-center'>
+                            <FaUserFriends size={24} className='mr-2 text-blue-500'/>
+                            <span>Max Capacity:</span>
+                        </div>
+                        <span>{room.max_capacity}</span>
+                    </div>
+                    {room.discount > 0 && (
+                        <div className='flex justify-between items-center text-green-600 font-semibold'>
+                            <div className='flex items-center'>
+                                <FaTag size={24} className='mr-2'/>
+                                <span>Discount:</span>
+                            </div>
+                            <span>{room.discount}$</span>
+                        </div>
+                    )}
+                </div>
             </div>
-         </div>
-         <div className='p-4'>
-            <Button fullWidth>
-               <Link
-                  href={`/rooms/${room.id}`}
-                  className='flex items-center justify-center w-full'
-               >
-                  <FaEye size={24} className='mr-2' />
-                  <span>Details</span>
-               </Link>
-            </Button>
-         </div>
-      </div>
-   );
+            <div className='p-4'>
+                <Link
+                    href={`/rooms/${room.id}`}
+                    className="group relative inline-flex w-full items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
+                >
+                <span
+                    className="relative flex w-full items-center justify-center gap-2 rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
+                    <FaEye className="h-5 w-5"/>
+                    <span className="text-sm font-semibold">Details</span>
+                </span>
+                </Link>
+            </div>
+        </div>
+    );
 }
